327b35695d4bc4508da2b8eb8eb4a043016c06d9
Andrew Clark <andrewscomputer@Andrews-MacBook-Air.local>
2020-10-28 16:39:32 -0600

    DB Set up maps started

--- controllers/pop.js
+++ controllers/pop.js
@@ -1,7 +1,18 @@
+const Poppy = require('../models/poppy');
+
 module.exports = {
     about,
-    store
+    store,
+    newForm,
+    create,
+    home,
+    showMap,
+    users,
   };
+
+function home(req, res) {
+  res.render('home')
+}
 
   function about(req, res) {
     res.render('about')
@@ -10,3 +21,29 @@ module.exports = {
 function store(req, res) {
     res.render('store')
 }
+
+function newForm(req, res) {
+  res.render('newForm')
+}
+
+function create(req, res) {
+  const poppy = new Poppy(req.body);
+  poppy.save(function(err) {
+    if (err) { return err } else {
+    res.json(req.body);
+    console.log(poppy);
+    }
+  });
+}
+
+function showMap(req, res) {
+  res.render('map')
+}
+
+function users(req, res) {
+  let query = Poppy.find({});
+  query.exec(function(err, users){
+      if(err) res.send(err);
+      res.json(users)
+  });
+};
